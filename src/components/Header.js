972176478c33fb8c1861d2453e601b4a5b5412f1
Peter Kang <kangguolong@gmail.com>
2020-09-21 10:07:44 +1200

    Header lost content issue.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => (
     <Jumbotron>
         <h3 className="display-4">Westpac Frontend Demo</h3>
         <br></br>
-        <p className="lead">This sample React project is used for </p>
+        <p className="lead">This sample React project is used for technical interview.</p>
     </Jumbotron>
 );
 
